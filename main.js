f5fcdbddf37d3ca3d31440c74d62d83631ffa8bd
Nattaparin <nattaparin455@gmail.com>
2022-07-17 17:32:24 +0700

    5.6

--- main.js
+++ main.js
@@ -1,58 +1,22 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Shoes',
-            brand: 'SE331',
-            // image: './assets/images/socks_green.jpg',
-            // inStock: true,
-            inventory: 100,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
-            ],
-            cart: 0,
-            SelectedVariant: 0,
-            onSale: true,
+            cart: [0,0],
             premium: true
-
         }
     },
     methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
-        },
-        updateVariant(index) {
-            this.SelectedVariant = index;
+        updateCart(id) {
+            if (id==2234) {
+                this.cart[0] += 1;
+            } else 
+                this.cart[1] += 1;
         },
 
-    },
-    computed: {
-
-        title() {
-            return this.brand + ' ' + this.product
-        },
-
-        image() {
-            return this.variants[this.SelectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.SelectedVariant].quantity
-        },
-        onSaleja() {
-            return this.brand + ' is on sale ' + this.product
-        },
-        shipping() {
-            if (this.premium) {
-                return 'Free'
-            }
-            return 30
+        removeFromCart() {
+            this.cart.splice(0,1,0)
+            this.cart.splice(1,1,0)
         }
 
-
     }
-
 })
